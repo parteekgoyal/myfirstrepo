6e081222d773461ee81c7ee5fc99f9c282c36ba2
parteekgoyal <51223818+parteekgoyal@users.noreply.github.com>
2025-07-29 22:55:28 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,34 @@
+console.log("Welcome to apna college");
+alert("Parteek Goyal");
+console.log("First post");
+
+fullname = "Parteek Goyal";
+console.log(fullname);
+age = 32;
+price = 1.1;
+a = null;
+y = undefined;
+
+$fullname = "Parteek";
+_fullname = "Parteek";
+
+const student ={age :32,
+    class: "MCA",
+    fullname:"Misha Goyal"
+};
+
+console.log(student);
+
+student["fullname"] = "Ysh Goyal";
+
+console.log(student["fullname"]);
+
+const product = {
+    label : "Pen",
+    rating: 4,
+    dealoftheday: true
+};
+
+console.log(product["label"]);
+console.log(typeof product);
+console.log(typeof product["label"]);
